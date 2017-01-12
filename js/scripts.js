db210a4001b8abcceae1ab6f2bd170b47eae789e
Karlos G liberal <karlosgliberal@gmail.com>
2017-01-12 13:43:50 +0100

    Texto del manifiesto

--- js/scripts.js
+++ js/scripts.js
@@ -1007,6 +1007,13 @@ $(document).ready(function() {
             document.body.appendChild(script);
         }
     }
+    var ttid = "killminusnueve";
+    var response = $.getJSON("https://query.yahooapis.com/v1/public/yql?q=select%20content%20from%20html%20where%20url%3D%22https%3A%2F%2Ftwitter.com%2F"+ttid+"%22%20and%20xpath%3D%27%2F%2Fli%5Bcontains(%40class%2C%22ProfileNav-item--tweets%22)%5D%2Fa%2Fspan%5Bcontains(%40class%2C%22ProfileNav-value%22)%5D%27&format=json&callback=");
+    response.success(function (data) {
+      $('.large.analizado').text(data.query.results.span*4*4);
+      $('.large.inculpados').text(data.query.results.span);
+        //alert(data.query.results.span);
+    });
 
 });
 
